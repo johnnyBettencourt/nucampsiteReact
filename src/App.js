b9c4ac8a2376277096424e140f65a398a4bcd10a
johnnyBettencourt <johnnybettencourt75@gmail.com>
2023-04-19 07:36:12 -0700

    3. Exercise: The App Component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import NucampLogo from './app/assets/img/logo.png';
 
 function App() {
   return (
     <div className="App">
-      Welcome to my Nucampsite React Edition
+      <Navbar dark color='primary' sticky='top' expand='md'>
+          <Container>
+              <NavbarBrand href='/'>
+                  <img src={NucampLogo} alt='nucamp logo' />
+              </NavbarBrand>
+          </Container>
+      </Navbar>
+      I'm ready for workshop!
     </div>
   );
 }
