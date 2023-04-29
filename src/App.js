6d31c15512f94f82061a6dd83f9a6477ca32eb6c
johnnyBettencourt <johnnybettencourt75@gmail.com>
2023-04-29 06:44:54 -0700

    Week 2 Workshop Assignment: Task 2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
@@ -13,12 +14,13 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path ='contact' element={<ContactPage />} />
         <Route path ='directory' element={<CampsitesDirectoryPage />} />
         <Route
           path='directory/:campsiteId'
           element={<CampsiteDetailPage />}
         />
+        <Route path ='About' element={<AboutPage />} />
+        <Route path ='contact' element={<ContactPage />} />
       </Routes>
       <Footer />
     </div>
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,8 +1,10 @@
 import { Col, Row, Container, Card, CardBody, CardHeader } from "reactstrap";
+import SubHeader from '../components/SubHeader';
 
 const AboutPage = () => {
     return (
         <Container>
+            <SubHeader current='About Us'/>
             <Row className='row-content'>
                 <Col sm='6'>
                     <h3>Our Mission</h3>
@@ -19,7 +21,7 @@ const AboutPage = () => {
                 </Col>
                 <Col sm='6'>
                     <Card>
-                        <CardHeader>
+                        <CardHeader className='bg-primary text-white'>
                             <h3>Facts at a Glance</h3>
                         </CardHeader>
                         <CardBody>
