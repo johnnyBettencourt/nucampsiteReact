cb32685504d766e151d319209af05addbad2aa93
johnnyBettencourt <johnnybettencourt75@gmail.com>
2023-04-28 21:02:35 -0700

    Exercise: CampsiteDetailPage with useParams()

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,25 +1,12 @@
-import { Container, Row, Col } from "reactstrap";
-import CampsiteDetail from '../features/campsites/CampsiteDetail';
+import { Container, } from "reactstrap";
 import CampsiteList from "../features/campsites/CampsitesList";
-import { selectCampsiteById } from "../features/campsites/campsitesSlice";
-import { useState } from "react";
 
 const CampsitesDirectoryPage = () => {
-    const [campsiteId, setCampsiteId] = useState(0);
-    const selectedCampsite = selectCampsiteById(campsiteId);
-
     return (
         <Container>
-            <Row>
-                <Col sm='5' md='7'>
-                    <CampsiteList setCampsiteId={setCampsiteId}/>
-                </Col>
-                <Col sm='7' md='5'>
-                    <CampsiteDetail campsite={selectedCampsite} />
-                </Col>
-            </Row>
+                    <CampsiteList />
         </Container>
-    )
+    );
 };
 
 export default CampsitesDirectoryPage;
