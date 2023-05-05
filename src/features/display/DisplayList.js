f31293f2865281c664f5905533de66b07486a63f
johnnyBettencourt <johnnybettencourt75@gmail.com>
2023-05-05 16:26:00 -0700

    Exercise: AnimatedDisplayCard

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,5 +1,5 @@
 import { Col, Row } from "reactstrap";
-import DisplayCard from './DisplayCard';
+import AnimatedDisplayCard from './AnimatedDisplayCard';
 import { selectFeaturedPartner } from "../partners/partnersSlice";
 import { selectFeaturedCampsite } from "../campsites/campsitesSlice";
 import { selectFeaturedPromotion } from "../promotions/PromotionsSlice";
@@ -10,9 +10,11 @@ const DisplayList = () =>{
         <Row>
             {items.map((item, idx) => {
                 return (
-                    <Col md className='m-1' key={idx}>
-                        <DisplayCard item={item} />
-                    </Col>
+                    item && (
+                        <Col md className='m-1' key={idx}>
+                            <AnimatedDisplayCard item={item} />
+                        </Col>
+                    )
                 );
             })}
         </Row>
